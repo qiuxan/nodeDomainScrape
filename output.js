4e4ed061a7e6f62830513bb2ad3fbec6e7d6b5cd
qiuxan <qiuxan@gmail.com>
2022-01-01 21:48:35 +1100

    get 2 pages

---
+++ output.js
@@ -0,0 +1,213 @@
+[
+    {
+        address: '22 Cashmere Crescent,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$847,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD AT AUCTION 22 DEC 2021'
+    },
+    {
+        address: '4 Rogers Close,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$1,256,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 20 DEC 2021'
+    },
+    {
+        address: '30 Lorikeet Drive,  BERWICK VIC 3806',
+        agent: 'Peter Watson',
+        price: '$1,000,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 20 DEC 2021'
+    },
+    {
+        address: '4 Kerry Close,  BERWICK VIC 3806',
+        agent: 'Matt Doyle',
+        price: '$1,114,400',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 17 DEC 2021'
+    },
+    {
+        address: '3 Santa Monica Place,  BERWICK VIC 3806',
+        agent: 'Gabriel Nigro',
+        price: '$840,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 17 DEC 2021'
+    },
+    {
+        address: '54 Edrington Park Drive,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$1,200,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '17 Matheson Court,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$1,175,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '2 Howitt Court,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$800,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '10 Norwich Drive,  BERWICK VIC 3806',
+        agent: 'Anne Haynes',
+        price: '$810,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 13 DEC 2021'
+    },
+    {
+        address: '7 Blake Street,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$955,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 10 DEC 2021'
+    },
+    {
+        address: '105 Wurundjeri Boulevard,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$950,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 08 DEC 2021'
+    },
+    {
+        address: '125 Bellevue Drive,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$770,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 08 DEC 2021'
+    },
+    {
+        address: '47 Streeton Way,  BERWICK VIC 3806',
+        agent: 'Gerard McRae',
+        price: '$1,080,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 08 DEC 2021'
+    },
+    {
+        address: '24 Glenn Erin Way,  BERWICK VIC 3806',
+        agent: 'Hayley Taufa',
+        price: '$835,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD AT AUCTION 08 DEC 2021'
+    },
+    {
+        address: '55 Marsh Grove,  BERWICK VIC 3806',
+        agent: 'Hayley Taufa',
+        price: '$937,500',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 08 DEC 2021'
+    },
+    {
+        address: '22 Cashmere Crescent,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$847,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD AT AUCTION 22 DEC 2021'
+    },
+    {
+        address: '4 Rogers Close,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$1,256,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 20 DEC 2021'
+    },
+    {
+        address: '30 Lorikeet Drive,  BERWICK VIC 3806',
+        agent: 'Peter Watson',
+        price: '$1,000,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 20 DEC 2021'
+    },
+    {
+        address: '4 Kerry Close,  BERWICK VIC 3806',
+        agent: 'Matt Doyle',
+        price: '$1,114,400',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 17 DEC 2021'
+    },
+    {
+        address: '3 Santa Monica Place,  BERWICK VIC 3806',
+        agent: 'Gabriel Nigro',
+        price: '$840,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 17 DEC 2021'
+    },
+    {
+        address: '54 Edrington Park Drive,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$1,200,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '17 Matheson Court,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$1,175,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '2 Howitt Court,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$800,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 16 DEC 2021'
+    },
+    {
+        address: '10 Norwich Drive,  BERWICK VIC 3806',
+        agent: 'Anne Haynes',
+        price: '$810,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 13 DEC 2021'
+    },
+    {
+        address: '7 Blake Street,  BERWICK VIC 3806',
+        agent: 'Debbie Brettoner',
+        price: '$955,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 10 DEC 2021'
+    },
+    {
+        address: '105 Wurundjeri Boulevard,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$950,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 08 DEC 2021'
+    },
+    {
+        address: '125 Bellevue Drive,  BERWICK VIC 3806',
+        agent: 'Eric Zhang',
+        price: '$770,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD PRIOR TO AUCTION 08 DEC 2021'
+    },
+    {
+        address: '47 Streeton Way,  BERWICK VIC 3806',
+        agent: 'Gerard McRae',
+        price: '$1,080,000',
+        bedroomNumber: '5 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 08 DEC 2021'
+    },
+    {
+        address: '24 Glenn Erin Way,  BERWICK VIC 3806',
+        agent: 'Hayley Taufa',
+        price: '$835,000',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD AT AUCTION 08 DEC 2021'
+    },
+    {
+        address: '55 Marsh Grove,  BERWICK VIC 3806',
+        agent: 'Hayley Taufa',
+        price: '$937,500',
+        bedroomNumber: '4 Beds',
+        soldDate: 'SOLD BY PRIVATE TREATY 08 DEC 2021'
+    }
+]
+
--- scrape.js
+++ scrape.js
@@ -1,34 +1,23 @@
 const puppeteer = require('puppeteer');
-
-
-(async () => {
+const getSoldData = (async (url, urlPage2) => {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const url = "https://www.domain.com.au/sold-listings/berwick-vic-3806/house/?bedrooms=4-any&excludepricewithheld=1";
-
+    let page = await browser.newPage();
+    // const url = "https://www.domain.com.au/sold-listings/berwick-vic-3806/house/?bedrooms=4-any&excludepricewithheld=1";
     // const VIEWPORT = { width: 1360, height: 780 }
     // await page.setViewport(VIEWPORT);
-
     await page.goto(url, { waitUntil: 'domcontentloaded' });
-
-
-    const agents = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-branding"]>*:nth-child(2)>span:first-child, [data-testid="listing-card-branding"]>*:nth-child(1) >span:first-child')).map((agent) => agent.innerText.replace(/\n/, " "))).filter(String)
+    let agents = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-branding"]>*:nth-child(2)>span:first-child, [data-testid="listing-card-branding"]>*:nth-child(1) >span:first-child')).map((agent) => agent.innerText.replace(/\n/, " "))).filter(String)
+    );
+    let addresses = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="address-wrapper"]')).map((address) => address.innerText.replace(/\n/, " "))).filter(String)
+    );
+    let prices = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-price"]')).map((price) => price.innerText.replace(/\n/, " "))).filter(String)
+    );
+    let soldDates = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-tag"]')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
+    );
+    let bedroomNumbers = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="property-features-feature"]:first-child')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
     );
 
-    const addresses = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="address-wrapper"]')).map((address) => address.innerText.replace(/\n/, " "))).filter(String)
-    );
-
-    const prices = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-price"]')).map((price) => price.innerText.replace(/\n/, " "))).filter(String)
-    );
-
-    const soldDates = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-tag"]')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
-    );
-
-    const bedroomNumbers = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="property-features-feature"]:first-child')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
-
-    );
-
-    var recentSoldData = {}
+    var recentSoldData = [];
 
     for (let i = 0; i < soldDates.length; i++) {
         let tempInfomation = {
@@ -38,11 +27,50 @@ const puppeteer = require('puppeteer');
             bedroomNumber: bedroomNumbers[i],
             soldDate: soldDates[i],
         };
-        recentSoldData = { ...recentSoldData, ...{ [i + 1]: tempInfomation } };
+        recentSoldData = [...recentSoldData, tempInfomation];
     }
 
-    console.log(recentSoldData);
 
     // console.log(agents.length + ' agents', agents, addresses.length + ' addresses:', addresses, prices.length + ' prices:', prices, soldDates.length + ' Sold Dates:', soldDates, bedroomNumbers.length + " bedroom numbers " + bedroomNumbers);
     await browser.close();
-})();
+    // console.log(recentSoldData);
+
+
+    const browser2 = await puppeteer.launch();
+
+
+    page = await browser2.newPage();
+    await page.goto(urlPage2, { waitUntil: 'domcontentloaded' });
+
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    agents = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-branding"]>*:nth-child(2)>span:first-child, [data-testid="listing-card-branding"]>*:nth-child(1) >span:first-child')).map((agent) => agent.innerText.replace(/\n/, " "))).filter(String)
+    );
+    addresses = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="address-wrapper"]')).map((address) => address.innerText.replace(/\n/, " "))).filter(String)
+    );
+    prices = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-price"]')).map((price) => price.innerText.replace(/\n/, " "))).filter(String)
+    );
+    soldDates = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="listing-card-tag"]')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
+    );
+    bedroomNumbers = await page.evaluate(() => (Array.from(document.querySelectorAll('[data-testid="listing-card-wrapper-premiumplus"] [data-testid="property-features-feature"]:first-child')).map((data) => data.innerText.replace(/\n/, " "))).filter(String)
+    );
+
+    for (let i = 0; i < soldDates.length; i++) {
+        let tempInfomation = {
+            address: addresses[i],
+            agent: agents[i],
+            price: prices[i],
+            bedroomNumber: bedroomNumbers[i],
+            soldDate: soldDates[i],
+        };
+        recentSoldData = [...recentSoldData, tempInfomation];
+    }
+
+    await browser2.close();
+
+    console.log("There are " + recentSoldData.length + " houses recently sold :", recentSoldData);
+
+    // console.log(agents.length + ' agents', agents, addresses.length + ' addresses:', addresses, prices.length + ' prices:', prices, soldDates.length + ' Sold Dates:', soldDates, bedroomNumbers.length + " bedroom numbers " + bedroomNumbers);
+
+    // return recentSoldData;
+})("https://www.domain.com.au/sold-listings/berwick-vic-3806/house/?bedrooms=4-any&excludepricewithheld=1", "https://www.domain.com.au/sold-listings/berwick-vic-3806/house/?bedrooms=4-any&excludepricewithheld=1&page=2");
+
